c88254bf5d39aef1fc3dd316eba2cb90e135e5f2
Rays91546 <mz.rainbowgem2000@gmail.com>
2016-10-02 16:46:20 -0700

    Add files via upload

---
+++ webPageLoginJS.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){ 
+
+         $('button').click(function(){
+
+         var passwords = document.getElementById('passwords').value;
+         var ids = document.getElementById('idNumber').value;
+
+         $('#pass').val(passwords);
+         $('#id')
+        });
+    });	
